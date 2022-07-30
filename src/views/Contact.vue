f1a2ad93e48a1a8bc29658288fce6bede2693529
Didiloy <didiloy2001@gmail.com>
2022-07-30 16:08:56 +0200

    init

---
+++ src/views/Contact.vue
@@ -0,0 +1,47 @@
+<script>
+import feather from "feather-icons";
+import ContactDetails from "@/components/contact/ContactDetails.vue";
+
+export default {
+  components: {
+    ContactDetails,
+  },
+  data: () => {
+    return {
+      contacts: [
+        {
+          id: 1,
+          name: "dylan.loya@proton.me",
+          icon: "mail",
+        },
+        {
+          id: 2,
+          name: "06 42 34 78 55",
+          icon: "phone",
+        },
+        {
+          id: 3,
+          name: "https://github.com/Didiloy",
+          icon: "github",
+        },
+      ],
+    };
+  },
+  mounted() {
+    feather.replace();
+  },
+  updated() {
+    feather.replace();
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <div
+    class="container mx-auto flex flex-col-reverse md:flex-row py-5 md:py-10 md:mt-10"
+  >
+    <!-- Contact details -->
+    <ContactDetails :contacts="contacts" />
+  </div>
+</template>
