436f75d6eba03bdd71cc92982ef7cd1a99718b5b
Didiloy <didiloy2001@gmail.com>
2022-07-31 17:20:39 +0200

    added projects details

--- src/data/projects.js
+++ src/data/projects.js
@@ -50,7 +50,7 @@ const projects = [{
             objectivesDetails: "Have recommendations based on what you have already read ! ",
             technologies: [{
                 title: "Tools & Technologies",
-                techs: ["Vue", "Javascript", "I18n", "Vuetify"],
+                techs: ["Vue", "Javascript", "I18n", "Vuetify", "Google Book Api"],
             }, ],
             projectDetailsHeading: "Challenge",
             projectDetails: [{
@@ -64,18 +64,87 @@ const projects = [{
         title: "MoVueDb",
         category: "Web Application",
         img: require("@/assets/images/movuedb.png"),
+        singleProjectHeader: {
+            singleProjectTitle: "MoVueDb",
+            singleProjectDate: "2022",
+            singleProjectTag: "Web Application",
+        },
+        projectImages: [{
+            id: 1,
+            title: "MoVueDb",
+            img: require("@/assets/images/movuedb.png"),
+        }, ],
+        projectInfo: {
+            objectivesHeading: "Objective",
+            objectivesDetails: "Search movie or TV shows and see details about them. Watch trailer and add movies to your favorite list !",
+            technologies: [{
+                title: "Tools & Technologies",
+                techs: ["Vue", "Javascript", "Materialize CSS"],
+            }, ],
+            projectDetailsHeading: "Challenge",
+            projectDetails: [{
+                id: 1,
+                details: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.",
+            }, ],
+        },
     },
     {
         id: 4,
         title: "Python Library app",
         category: "Python Application",
         img: require("@/assets/images/python_library_app.png"),
+        singleProjectHeader: {
+            singleProjectTitle: "Python Library App",
+            singleProjectDate: "2021",
+            singleProjectTag: "Python Desktop Application",
+        },
+        projectImages: [{
+            id: 1,
+            title: "Python Library App",
+            img: require("@/assets/images/python_library_app.png"),
+        }, ],
+        projectInfo: {
+            objectivesHeading: "Objective",
+            objectivesDetails: "Have recommendations based on what you have already read ! ",
+            technologies: [{
+                title: "Tools & Technologies",
+                techs: ["Python", "PyQt5", "Google Book Api"],
+            }, ],
+            projectDetailsHeading: "Challenge",
+            projectDetails: [{
+                id: 1,
+                details: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.",
+            }, ],
+        },
     },
     {
         id: 5,
         title: "Programme de Marche",
         category: "Java Application",
         img: require("@/assets/images/programme_marche.png"),
+        singleProjectHeader: {
+            singleProjectTitle: "Programme de Marche",
+            singleProjectDate: "2021",
+            singleProjectTag: "Java Desktop Application",
+        },
+        projectImages: [{
+            id: 1,
+            title: "Programme de marche",
+            img: require("@/assets/images/programme_marche.png"),
+        }, ],
+        projectInfo: {
+            objectivesHeading: "Objective",
+            objectivesDetails: "Be able to see graphics about the evolution of your wheight, time you spent walking and distance",
+            technologies: [{
+                title: "Tools & Technologies",
+                techs: ["Java", "JFreeCharts", "Swing"],
+            }, ],
+            projectDetailsHeading: "Challenge",
+            projectDetails: [{
+                id: 1,
+                details: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.",
+            }, ],
+        },
     },
 ];
 
